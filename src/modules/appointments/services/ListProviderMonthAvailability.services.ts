b0eb299b9f31cc0a1074d0d9ba3f10e844c630ca
Filipe Freitas <filipe.freitas@live.com>
2020-10-01 22:13:09 -0300

    refactor: check day availability

--- src/modules/appointments/services/ListProviderMonthAvailability.services.ts
+++ src/modules/appointments/services/ListProviderMonthAvailability.services.ts
@@ -2,6 +2,7 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@appointments/repositories/IAppointmentsRepository';
 
@@ -36,9 +37,27 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      {
+        length: numberOfDaysInMonth,
+      },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
