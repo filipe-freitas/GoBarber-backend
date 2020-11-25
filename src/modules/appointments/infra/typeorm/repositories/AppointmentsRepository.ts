63c98e914abc66d7ec6b0acbfaf6094faf04a239
Filipe Freitas <filipe.freitas@live.com>
2020-11-24 21:29:30 -0300

    fix: amend the search for provider's appointments
    ammed the date string for the search

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -73,7 +73,7 @@ class AppointmentRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_char(${dateFieldName}, 'DD-MM-YYYY') = ${parsedDay}-${parsedMonth}-${year}`,
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
