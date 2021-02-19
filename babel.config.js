dfdc3e001bd63541e6b5398e2c23c1e4478eda9f
Filipe Freitas <filipe.freitas@live.com>
2021-02-18 21:29:27 -0300

    chore: add babel as a transpiler option

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@appointments': './src/modules/appointments',
+          '@notifications': './src/modules/notifications',
+          '@users': './src/modules/users',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
